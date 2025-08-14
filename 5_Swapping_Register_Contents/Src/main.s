222e27fe2a04fc9d57e4fe467ee33c2e79b10ada
Emma <73042133+Ninjafroggyy@users.noreply.github.com>
2025-08-14 16:08:26 +0100

    Updated project 5

--- 5_Swapping_Register_Contents/Src/main.s
+++ 5_Swapping_Register_Contents/Src/main.s
@@ -18,26 +18,14 @@ sum		.req	r3		// Rename register 3 as sum
 			.global __main
 
 __main:
-			ldr r0, =A				// Point r0 to memory location A
-			mov r1, #5				// Put 5 into registry 1
-			str r1, [r0]			// Store content of r1 (5) into the memory address (A) that r0 is pointing at
+			ldr	r0, =0xBABEFACE		// Load 32 bit value
+			ldr r1, =0xDEADBEEF		// Load 32 bit value
 
-			ldr r0, =B				// Point r0 to memory location B
-			mov r1, #4				// Put 4 into registry 1
-			str r1, [r0]			// Store content of r1 (4) into the memory address (B) that r0 is pointing at
-
-			ldr r0, =C				// Point r0 to memory location C
-			mov r1, #200			// Put 200 into registry 1
-			str r1, [r0]			// Store content of r1 (5) into the memory address (C) that r0 is pointing at
+			eor	r0, r0, r1			// Exclusive OR store in r0
+			eor	r1, r0, r1			// Exclusive OR store in r1
+			eor r0, r0, r1			// Exclusive OR store in r0
 stop:
 			b stop
-
-
-			.section .data			// Stores to RAM location
-
-A:			.space		4			// Allocate 4 bytes of memory filled with zeroes
-B:			.space		4			// Allocate 4 bytes of memory filled with zeroes
-C:			.space		4			// Allocate 4 bytes of memory filled with zeroes
 
 
 			.align
